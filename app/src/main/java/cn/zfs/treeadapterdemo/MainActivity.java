cee9c1219e80c456a1a2cc6530ba59f5642eb481
fszeng2011 <zeng_fs@yeah.net>
2017-10-20 11:58:04 +0800

    初建

---
+++ app/src/main/java/cn/zfs/treeadapterdemo/MainActivity.java
@@ -0,0 +1,107 @@
+package cn.zfs.treeadapterdemo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import cn.zfs.treeadapter.Node;
+import cn.zfs.treeadapter.TreeAdapter;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ListView lv = new ListView(this);
+        setContentView(lv, new ViewGroup.LayoutParams(-1, -1));
+        List<Item> list = new ArrayList<>();
+        list.add(new Item(0, 0, 0, true, "Android"));
+        list.add(new Item(1, 0, 1, false, "Service"));
+        list.add(new Item(2, 0, 1, false, "Activity"));
+        list.add(new Item(3, 0, 1, false, "Receiver"));
+        list.add(new Item(4, 0, 0, false, "Java Web"));
+        list.add(new Item(5, 4, 1, false, "CSS"));
+        list.add(new Item(6, 4, 1, false, "Jsp"));
+        list.add(new Item(7, 4, 1, false, "Html"));
+        list.add(new Item(8, 7, 2, false, "p"));
+        MyAdapter adapter = new MyAdapter(list);
+        adapter.setOnInnerItemClickListener(new TreeAdapter.OnInnerItemClickListener<Item>() {
+            @Override
+            public void onClick(Item node) {
+                Toast.makeText(MainActivity.this, "click: " + node.name, Toast.LENGTH_SHORT).show();
+            }
+        });
+        lv.setAdapter(adapter);
+    }
+
+    private class MyAdapter extends TreeAdapter<Item> {
+        MyAdapter(List<Item> nodes) {
+            super(nodes);
+        }
+
+        @Override
+        protected Holder<Item> getHolder() {
+            return new Holder<Item>() {
+                private ImageView iv;
+                private TextView tv;
+                private View rootView;
+
+                @Override
+                protected void setData(Item node) {
+                    iv.setVisibility(node.hasChild() ? View.VISIBLE : View.INVISIBLE);
+                    iv.setBackgroundResource(node.isExpand ? R.mipmap.expand : R.mipmap.fold);
+                    RelativeLayout.LayoutParams params = (RelativeLayout.LayoutParams) iv.getLayoutParams();
+                    params.leftMargin = (node.level + 1) * dip2px(20);
+                    iv.setLayoutParams(params);
+                    tv.setText(node.name);
+                    if (node.hasChild()) {
+                        switch(node.level) {
+                            case 0:
+                                rootView.setBackgroundColor(0xff30C251);
+                                break;
+                            case 1:
+                                rootView.setBackgroundColor(0xff446F91);
+                                break;
+                        }
+                    } else {
+                        rootView.setBackgroundColor(0xffE36209);
+                    }                 
+                }
+
+                @Override
+                protected View createConvertView() {
+                    rootView = View.inflate(MainActivity.this, R.layout.item_tree_list, null);
+                    iv = (ImageView) rootView.findViewById(R.id.ivIcon);
+                    tv = (TextView) rootView.findViewById(R.id.tvName);
+                    return rootView;
+                }
+            };
+        }
+    }
+
+    /**
+     * 根据手机的分辨率从 dip 的单位 转成为 px(像素) 
+     */
+    public int dip2px(float dpValue) {
+        final float scale = getResources().getDisplayMetrics().density;
+        return (int) (dpValue * scale + 0.5f);
+    }
+    
+    private class Item extends Node<Item> {
+        String name;
+
+        Item(int id, int pId, int level, boolean isExpand, String name) {
+            super(id, pId, level, isExpand);
+            this.name = name;
+        }
+    }
+}
---
+++ treeadapter/src/main/java/cn/zfs/treeadapter/TreeAdapter.java
@@ -0,0 +1,167 @@
+package cn.zfs.treeadapter;
+
+import android.util.SparseIntArray;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * 时间: 2017/10/19 16:12
+ * 作者: 曾繁盛
+ * 邮箱: 43068145@qq.com
+ * 功能: 树结构ListView适配器。
+ */
+
+public abstract class TreeAdapter<T extends Node<T>> extends BaseAdapter {
+    private List<T> totalNodes = new ArrayList<>();
+    private List<T> showNodes = new ArrayList<>();
+    private List<T> firstLevelNodes = new ArrayList<>();
+    private SparseIntArray addedChildNodeIds = new SparseIntArray();
+    private OnInnerItemClickListener<T> listener;
+
+    public interface OnInnerItemClickListener<T> {
+        void onClick(T node);
+    }
+    
+    public TreeAdapter(List<T> nodes) {
+        setNodes(nodes);        
+    }
+    
+    public void setOnInnerItemClickListener(OnInnerItemClickListener<T> listener) {
+        this.listener = listener;
+    }
+    
+    public void setNodes(List<T> nodes) {
+        if (nodes != null && !nodes.isEmpty()) {
+            totalNodes = nodes;
+            //过滤出显示的节点
+            showNodes.clear();
+            initNodes();
+            addedChildNodeIds.clear();
+            showNodes.addAll(firstLevelNodes);
+            filterShowAndSortNodes();
+            notifyDataSetChanged();
+        }
+    }
+    
+    private void initNodes() {
+        //先循环一次，获取最小的level
+        Integer level = null;
+        for (T node : totalNodes) {
+            if (level == null || level > node.level) {
+                level = node.level;
+            }
+        }
+        for (T node : totalNodes) {
+            //过滤出最外层
+            if (node.level == level) {
+                firstLevelNodes.add(node);
+            }
+            //给节点添加子节点并排序
+            for (T t : totalNodes) {
+                if (node.id == t.pId && node.level != t.level) {
+                    node.addChild(t);
+                }
+            }
+            if (node.hasChild()) {
+                Collections.sort(node.childNodes);
+            }
+        }    
+        Collections.sort(firstLevelNodes);
+    }
+    
+    private void filterShowAndSortNodes() {
+        for (int i = 0; i < showNodes.size(); i++) {
+            T node = showNodes.get(i);
+            int value = addedChildNodeIds.get(node.id);
+            if (value == 0 && node.isExpand && node.hasChild()) {
+                List<T> list = new ArrayList<>(showNodes);
+                list.addAll(i + 1, node.childNodes);
+                showNodes = list;
+                addedChildNodeIds.put(node.id, 1);
+                filterShowAndSortNodes();
+                break;
+            }
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return showNodes.size();
+    }
+
+    @Override
+    public T getItem(int position) {
+        return showNodes.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        Holder<T> holder;
+        if (convertView == null) {
+            holder = getHolder();
+        } else {
+            holder = (Holder<T>) convertView.getTag();
+        }
+        holder.setData(showNodes.get(position));
+        holder.position = position;
+        View view = holder.getConvertView();
+        view.setOnClickListener(clickListener);
+        return view;
+    }
+    
+    public abstract static class Holder<T> {
+        private View convertView;
+        int position;
+
+        public Holder() {
+            convertView = createConvertView();
+            convertView.setTag(this);
+        }
+
+        public View getConvertView() {
+            return convertView;
+        }
+        
+        /**
+         * 设置数据
+         */
+        protected abstract void setData(T node);
+
+        /**
+         * 创建界面
+         */
+        protected abstract View createConvertView();
+    }
+
+    protected abstract Holder<T> getHolder();
+    
+    private View.OnClickListener clickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            if (listener != null) {
+                Holder<T> holder = (Holder<T>) v.getTag();
+                T node = showNodes.get(holder.position);
+                if (node.hasChild()) {
+                    node.isExpand = !node.isExpand;
+                    showNodes.clear();
+                    addedChildNodeIds.clear();
+                    showNodes.addAll(firstLevelNodes);
+                    filterShowAndSortNodes();
+                    notifyDataSetChanged();
+                } else {
+                    listener.onClick(node);
+                }
+            }
+        }
+    };
+}
